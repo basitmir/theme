6f78c04094f398053fd5b3d7d16fe30288107da2
Basit Mir <iammirbasit@gmail.com>
2018-12-31 21:53:54 +0530

    save changes on 31

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 $(function(){
-   
-     
+
+
     new WOW().init();
    $(".face img").hover(function(){
       $(this).toggleClass("zoom");
    });
+   
+   $('.counter').counterUp({
+      delay: 10,
+      time: 2000
+  });
 });
